36959e0c5d5c214e8b1fa3a9bd68e72bcf6bca89
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2025-05-21 02:55:45 +0800

    init work

---
+++ fresh-project/dev.ts
@@ -0,0 +1,19 @@
+import { Builder } from "fresh/dev";
+import { tailwind } from "jsr:@freshpress/fresh-plugin-tailwind@0.0.1-alpha.8";
+//import { tailwind } from "@fresh/plugin-tailwind";
+import { app } from "./main.ts";
+
+// Pass development only configuration here
+const builder = new Builder({ target: "safari12" });
+
+// Example: Enabling the tailwind plugin for Fresh
+tailwind(builder, app, {});
+
+// Create optimized assets for the browser when
+// running `deno run -A dev.ts build`
+if (Deno.args.includes("build")) {
+  await builder.build(app);
+} else {
+  // ...otherwise start the development server
+  await builder.listen(app);
+}
